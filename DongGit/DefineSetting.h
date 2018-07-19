04a4933027eb137af6995a4b1ddb46136f93a382
Arthur <957980613@qq.com>
2018-07-20 00:50:37 +0800

    database add-ons

--- DongGit/DefineSetting.h
+++ DongGit/DefineSetting.h
@@ -3,6 +3,6 @@
 #define CURRENT_BRANCH ".git\\status\\Current_Branch"
 #define GIT_HEAD ".git\\"
 
-#define INFO_DB ".git\\db\\Infodb"
+#define INFO_DB ".git\\db\\Infodb.db"
 
 #define NONE_FILE_HASH "00000000000000000000000000000000"
--- DongGit/db_operate.cpp
+++ DongGit/db_operate.cpp
@@ -1,26 +1,130 @@
 #include "stdafx.h"
 #include "sqlite3.h"
+#include "callback.h"
 #include "db_operate.h"
 
 void File_Match_Insert(const string& name, const string& hash)
 {
-	// TODO:向FILE_MATCH表中插入数据
+	sqlite3* db;
+	int rc;
+	char *zErrMsg = nullptr;
+
+	rc = sqlite3_open(INFO_DB, &db);
+	if (rc)
+	{
+		string sql = "INSERT INTO TABLE FILE_MATCH VALUES ('" + name +"','"+hash+"','"+ NONE_FILE_HASH+"')";
+		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK)
+		{
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else 
+		{
+			fprintf(stdout, "Insert Successfully");
+		}
+		sqlite3_close(db);
+		return;
+	}
+	else
+	{
+		fprintf(stdout, "###ERROR_OPEN_DATABASE###");
+	}
 
 }
 
 void File_Match_Update(const string& name, const string& hash, const string& pre_hash)
 {
-	// TODO:向FILE_BRANCH表中更新数据
+	sqlite3* db;
+	int rc;
+	char *zErrMsg = nullptr;
+
+	rc = sqlite3_open(INFO_DB, &db);
+	if (rc)
+	{
+		string sql = "INSERT INTO TABLE FILE_MATCH VALUES ('" + name + "','" + hash + "','" + pre_hash + "')";
+		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK)
+		{
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else
+		{
+			fprintf(stdout, "Update Successfully");
+		}
+		sqlite3_close(db);
+		return;
+	}
+	else
+	{
+		fprintf(stdout, "###ERROR_OPEN_DATABASE###");
+	}
 }
+
+
+void Branch_Match_Adddisc(const string& disc)
+{
+
+}
+
 
 void Branch_Match_Insert(const string& name, const string& hash)
 {
-	// TODO:向BRANCH_MATCH表中插入数据
+	sqlite3* db;
+	int rc;
+	char *zErrMsg = nullptr;
+
+	rc = sqlite3_open(INFO_DB, &db);
+	if (rc)
+	{
+		string sql = "INSERT INTO TABLE BRANCH_MATCH VALUES ('" + name + "','" + hash + "','" + NONE_FILE_HASH+ "','"+"";
+		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK)
+		{
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else
+		{
+			fprintf(stdout, "Update Successfully");
+		}
+		sqlite3_close(db);
+		return;
+	}
+	else
+	{
+		fprintf(stdout, "###ERROR_OPEN_DATABASE###");
+	}
 }
 
 void Branch_Match_Update(const string& name, const string& hash, const string& pre_hash, int his_id)
 {
-	// TODO:向BRANCH_MATCH表中更新数据
+	sqlite3* db;
+	int rc;
+	char *zErrMsg = nullptr;
+
+	rc = sqlite3_open(INFO_DB, &db);
+	if (rc)
+	{
+		string sql = "INSERT INTO TABLE BRANCH_MATCH VALUES ('" + name + "','" + hash + "','" + NONE_FILE_HASH + "','" + ;
+		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		if (rc != SQLITE_OK)
+		{
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+		}
+		else
+		{
+			fprintf(stdout, "Update Successfully");
+		}
+		sqlite3_close(db);
+		return;
+	}
+	else
+	{
+		fprintf(stdout, "###ERROR_OPEN_DATABASE###");
+	}
 }
 
 string Branch_Search_Name(const string& hash)
