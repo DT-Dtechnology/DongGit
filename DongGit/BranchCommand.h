2e7bb39604c455a100adae4dd3cc15b66acbb9c9
DT-Dtechnology <DT-Dtechnology@outlook.com>
2018-07-20 22:37:30 +0800

    DongHaowen

--- DongGit/BranchCommand.h
+++ DongGit/BranchCommand.h
@@ -4,4 +4,4 @@ void branch_command();
 
 void branch_command(const string& to_branch);
 
-void branch_command(const string& from_branch, const string& to_branch)
+void branch_command(const string& from_branch, const string& to_branch);
