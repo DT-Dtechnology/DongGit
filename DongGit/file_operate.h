d1324b90def59aecaeec9dd79042c2fcf6857cae
文哲 李 <1094764471@qq.com>
2018-07-23 22:35:37 +0800

    get_file_name()

--- DongGit/file_operate.h
+++ DongGit/file_operate.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <windows.h>
 
 using std::string;
+using std::vector;
 
 // 获取当前的分支名称
 string get_current_branch();
@@ -12,3 +15,6 @@ void file_conflict(const string& branch1, const string& branch2);
 string get_branch_file_hash(const string& branch, const string& file);
 
 string get_Branch_Latest_Hash(const string& name);
+
+// 获取工作区下所有文件名
+void get_file_name(string path, vector<string> file);
